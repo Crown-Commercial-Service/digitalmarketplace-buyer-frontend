9f9c34222d5d9f0ddd5db197f0cc5431ce924d08
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-03-02 17:27:37 +0000

    Use search API instead of hitting elasticsearch directly

--- app/models.py
+++ app/models.py
@@ -1,9 +1,25 @@
 import requests
 import os
 from flask import json
-from elasticsearch import Elasticsearch
 
-es = Elasticsearch()
+
+api_url = os.getenv('DM_API_URL')
+api_access_token = os.getenv('DM_API_BEARER')
+search_url = os.getenv('DM_SEARCH_API_URL')
+search_access_token = os.getenv('DM_SEARCH_API_BEARER')
+
+if api_access_token is None:
+    print('Bearer token must be supplied in DM_API_BEARER')
+    raise Exception("DM_API_BEARER token is not set")
+if api_url is None:
+    print('API URL must be supplied in DM_API_URL')
+    raise Exception("DM_API_URL is not set")
+if search_access_token is None:
+    print('Bearer token must be supplied in DM_SEARCH_API_BEARER')
+    raise Exception("DM_SEARCH_API_BEARER token is not set")
+if search_url is None:
+    print('Search API URL must be supplied in DM_SEARCH_API_URL')
+    raise Exception("DM_SEARCH_API_URL is not set")
 
 
 def strip_services_wrapper(content):
@@ -12,110 +28,44 @@ def strip_services_wrapper(content):
 
 
 def get_service(service_id):
-    # TODO: Don't do these checks for every call - initialise once
-    access_token = os.getenv('DM_API_BEARER')
-    api_url = os.getenv('DM_API_URL')
-    if access_token is None:
-        print('Bearer token must be supplied in DM_API_BEARER')
-        raise Exception("DM_API_BEARER token is not set")
-    if api_url is None:
-        print('API URL must be supplied in DM_API_URL')
-        raise Exception("DM_API_URL is not set")
     url = api_url + "/services/" + service_id
     response = requests.get(
         url,
         headers={
-            "authorization": "Bearer {}".format(access_token)
+            "authorization": "Bearer {}".format(api_access_token)
         }
     )
     return strip_services_wrapper(response.content)
 
 
 def search_without_filters(query, start_from=0, size=10):
-    res = es.search(index="services",
-                    body={
-                        "from": start_from, "size": size,
-                        "fields": ["id", "serviceName", "lot",
-                                   "serviceSummary"],
-                        "query": {
-                            "multi_match": {
-                                "query": query,
-                                "fields": ["serviceName",
-                                           "serviceSummary",
-                                           "serviceFeatures",
-                                           "serviceBenefits"],
-                                "operator": "or"
-                            }
-                        }
-                        }
-                    )
-    return res
+    url = search_url + "/search?q=" + query
+    response = requests.get(
+        url,
+        headers={
+            "authorization": "Bearer {}".format(search_access_token)
+        }
+    )
+    return response.content
 
 
-def search_with_lot_filter(query, lot, start_from=0, size=10):
-    res = es.search(index="services",
-                    body={
-                        "from": start_from, "size": size,
-                        "fields": ["id", "serviceName", "lot",
-                                   "serviceSummary"],
-                        "query": {
-                            "filtered": {
-                                "query": {
-                                    "multi_match": {
-                                        "query": query,
-                                        "fields": ["serviceName",
-                                                   "serviceSummary",
-                                                   "serviceFeatures",
-                                                   "serviceBenefits"],
-                                        "operator": "or"
-                                    }
-                                },
-                                "filter": {
-                                    "term": {"lot": lot.lower()}
-                                }
-                            }
-                        }
-                    }
-                    )
-    return res
-
-
-def search_with_filters(query, args, start_from=0, size=10):
-    filters = []
-    for k, v in args.iteritems():
-        filters.append({"term": {k: v.lower()}})
-    res = es.search(index="services",
-                    body={
-                        "from": start_from, "size": size,
-                        "fields": ["id", "serviceName", "lot",
-                                   "serviceSummary"],
-                        "query": {
-                            "filtered": {
-                                "query": {
-                                    "multi_match": {
-                                        "query": query,
-                                        "fields": ["serviceName",
-                                                   "serviceSummary",
-                                                   "serviceFeatures",
-                                                   "serviceBenefits"],
-                                        "operator": "or"
-                                    }
-                                },
-                                "filter": {
-                                    "bool": {"must": filters}
-                                }
-                            }
-                        }
-                    }
-                    )
-    return res
+def search_with_filters(query, filters, start_from=0, size=10):
+    filter_string = ""
+    for k, v in filters.iteritems():
+        filter_string = filter_string + "&" + k + "=" + v.lower()
+    url = search_url + "/search?q=" + query + filter_string
+    response = requests.get(
+        url,
+        headers={
+            "authorization": "Bearer {}".format(search_access_token)
+        }
+    )
+    return response.content
 
 
 def search_for_service(keywords="", filters={}, start_from=0, size=10):
     query = keywords
-    if "lot" in filters:
-        return search_with_lot_filter(query, filters["lot"], start_from, size)
-    elif filters:
+    if filters:
         return search_with_filters(query, filters, start_from, size)
     else:
         return search_without_filters(query, start_from, size)
