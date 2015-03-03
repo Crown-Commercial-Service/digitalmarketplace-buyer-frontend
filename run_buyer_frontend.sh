816864c7e2f6790de81dadea2a300c74126bc368
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-03-03 10:30:16 +0000

    Add run script
    This script activates the virtual environment, checks for required environment variables, sets some sensible defaults for local dev if they do not exist, and starts the application.

---
+++ run_buyer_frontend.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ./venv/bin/activate
+echo "Virtual environment activated."
+
+# Use default environment vars for localhost if not already set
+export DM_API_URL=${DM_API_URL:=http://localhost:5000}
+export DM_API_BEARER=${DM_API_BEARER:=myToken}
+export DM_SEARCH_API_URL=${DM_SEARCH_API_URL:=http://localhost:5001}
+export DM_SEARCH_API_BEARER=${DM_SEARCH_API_BEARER:=myToken}
+
+echo "Environment variables in use:" 
+env | grep DM_
+
+python application.py runserver
