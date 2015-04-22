772a9ae9f3c443876d915c45b1a716ad5936b216
Tom Byers <tombaromba@gmail.com>
2015-04-22 17:24:30 +0100

    Add lot filters to SearchFilters class

--- app/presenters/search_presenters.py
+++ app/presenters/search_presenters.py
@@ -55,7 +55,56 @@ class SearchFilters(object):
     def __init__(self, blueprint=False, request={}):
         self.filter_groups = blueprint.config['FILTER_GROUPS']
         self.request_filters = self.__get_filters_from_request(request)
+        self.lot_filters = self.__get_lot_filters_from_request(request)
         self.__set_filter_states()
+
+    def __get_lot_filters_from_request(self, request):
+        """Returns data to construct lot filters from"""
+
+        def _get_url(keywords=None, lot=None):
+            params = []
+            if keywords is not None:
+                params.append('q=' + keywords)
+            if lot is not None:
+                params.append('lot=' + lot)
+            if len(params) == 0:
+                return '/search'
+            else:
+                return '/search?' + '&'.join(params)
+
+        lot_names = ['all', 'saas', 'paas', 'iaas', 'scs']
+        current_lot = request.args.get('lot', 'all')
+        keywords = request.args.get('q', None)
+        lot_filters = [
+            {
+                'isActive': False,
+                'label': u'All categories',
+                'url': _get_url(keywords=keywords)
+            },
+            {
+                'isActive': False,
+                'label': u'Software as a Service',
+                'url': _get_url(keywords=keywords, lot='saas')
+            },
+            {
+                'isActive': False,
+                'label': u'Platform as a Service',
+                'url': _get_url(keywords=keywords, lot='paas')
+            },
+            {
+                'isActive': False,
+                'label': u'Infrastructure as a Service',
+                'url': _get_url(keywords=keywords, lot='iaas')
+            },
+            {
+                'isActive': False,
+                'label': u'Specialist Cloud Services',
+                'url': _get_url(keywords=keywords, lot='scs')
+            }
+        ]
+        if current_lot != None:
+            lot_filters[lot_names.index(current_lot)]['isActive'] = True
+        return lot_filters
 
     def __get_filters_from_request(self, request):
         """Returns the filters applied to a search from the request object"""
@@ -64,7 +113,9 @@ class SearchFilters(object):
         filters = {}
         for key in request.args:
             if key != 'q':
-                filters[key] = request.args[key]
+                arg_value = request.args.get(key, None)
+                if arg_value != None:
+                    filters[key] = arg_value
         return filters
 
     def __set_filter_states(self):
