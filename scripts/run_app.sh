f321711b4170e0fc328bbf40c046a2bb905b0624
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2015-04-30 11:47:49 +0100

    Change environment variables for dmutils.apiclient

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -2,11 +2,10 @@
 source ./venv/bin/activate 2>/dev/null && echo "Virtual environment activated."
 
 # Use default environment vars for localhost if not already set
-export DM_API_URL=${DM_API_URL:=http://localhost:5000}
-export DM_BUYER_FRONTEND_API_AUTH_TOKEN=${DM_BUYER_FRONTEND_API_AUTH_TOKEN:=myToken}
+export DM_DATA_API_URL=${DM_DATA_API_URL:=http://localhost:5000}
+export DM_DATA_API_AUTH_TOKEN=${DM_DATA_API_AUTH_TOKEN:=myToken}
 export DM_SEARCH_API_URL=${DM_SEARCH_API_URL:=http://localhost:5001}
-export DM_BUYER_FRONTEND_SEARCH_API_AUTH_TOKEN=${DM_BUYER_FRONTEND_SEARCH_API_AUTH_TOKEN:=myToken}
-
+export DM_SEARCH_API_AUTH_TOKEN=${DM_SEARCH_API_AUTH_TOKEN:=myToken}
 echo "Environment variables in use:" 
 env | grep DM_
 
