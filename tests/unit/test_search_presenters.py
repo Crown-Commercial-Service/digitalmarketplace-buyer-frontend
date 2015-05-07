40ce0a65860cddc4fe4aaa93755037a376fa4b32
Tom Byers <tombaromba@gmail.com>
2015-05-07 16:51:02 +0100

    Add tests for highlighting

--- tests/unit/test_search_presenters.py
+++ tests/unit/test_search_presenters.py
@@ -2,7 +2,9 @@ import os
 import json
 import unittest
 
-from Mock import mock
+from flask import Markup
+from mock import Mock
+from app.presenters.search_presenters import SearchResults
 
 
 def _get_fixture_data():
@@ -10,7 +12,7 @@ def _get_fixture_data():
         os.path.join(os.path.dirname(__file__), "..")
     )
     fixture_path = os.path.join(
-        test_root, 'fixtures', 'search_fixture.json'
+        test_root, 'fixtures', 'search_results_fixture.json'
     )
     with open(fixture_path) as fixture_file:
         return json.load(fixture_file)
@@ -18,14 +20,62 @@ def _get_fixture_data():
 
 class TestSearchResults(unittest.TestCase):
 
+    def _get_service_result_by_id(self, results, id):
+        for result in results:
+            if result['id'] == id:
+                return result
+        return False
+
     def setUp(self):
-        self.fixture = _get_fixture_data()['services']
-        self.service = Service(self.fixture)
+        self.fixture = _get_fixture_data()
+        self.service = SearchResults(self.fixture)
 
     def tearDown(self):
         pass
 
     def test_search_results_is_set(self):
-        search_results = SearchResults(self.fixture)
-        self.assert
+        search_results_instance = SearchResults(self.fixture)
+        self.assertTrue(hasattr(search_results_instance, 'search_results'))
 
+    def test_highlighting_for_one_line_summary(self):
+        search_results_instance = SearchResults(self.fixture)
+        result_with_one_line_highlight = self._get_service_result_by_id(
+            search_results_instance.search_results, '4-G4-0871-001'
+        )
+        self.assertEquals(
+            result_with_one_line_highlight['serviceSummary'],
+            Markup(
+                u"Fastly <em>CDN</em> (Content Delivery Network) speeds up" +
+                u" delivery of your website and its content to your"
+            )
+        )
+
+    def test_highlighting_for_multiple_line_summary(self):
+        search_results_instance = SearchResults(self.fixture)
+        result_with_multi_line_highlight = self._get_service_result_by_id(
+            search_results_instance.search_results, '4-G1-0340-001'
+        )
+        self.assertEquals(
+            result_with_multi_line_highlight['serviceSummary'],
+            Markup(
+                u" Baycloud Systems or in a suitable Content Delivery"
+                u" Network (<em>CDN</em>) such as the Windows Azure" +
+                u" <em>CDN</em>. The <em>CDN</em> improves performance" +
+                u" by caching content at locations closest to visitors" +
+                u" to customer\u2019s websites. The Windows Azure" +
+                u" <em>CDN</em> is a managed service that is operated by" +
+                u" Microsoft and has a 99.99% monthly"
+            )
+        )
+
+    def test_highlighting_only_happens_on_service_summaries(self):
+        search_results_instance = SearchResults(self.fixture)
+        result_with_service_name_highlight = self._get_service_result_by_id(
+            search_results_instance.search_results, '5-G3-0279-010'
+        )
+        print "result_with_service_name_highlight:"
+        print result_with_service_name_highlight
+        self.assertEquals(
+            result_with_service_name_highlight['serviceName'],
+            "CDN VDMS"
+        )
