366d258f016c4099af32ccaf7cb3d76efba27d55
Tom Byers <tombaromba@gmail.com>
2015-05-14 16:16:52 +0100

    Correct the environment variables
    They will now match those used by DM Utils.

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -6,8 +6,8 @@ else
 fi
 
 # Use default environment vars for localhost if not already set
-export DM_API_URL=${DM_API_URL:=http://localhost:5000}
-export DM_BUYER_FRONTEND_API_AUTH_TOKEN=${DM_BUYER_FRONTEND_API_AUTH_TOKEN:=myToken}
+export DM_DATA_API_URL=${DM_DATA_API_URL:=http://localhost:5000}
+export DM_DATA_API_AUTH_TOKEN=${DM_DATA_API_AUTH_TOKEN:=myToken}
 export DM_SEARCH_API_URL=${DM_SEARCH_API_URL:=http://localhost:5001}
 export DM_SEARCH_API_AUTH_TOKEN=${DM_SEARCH_API_AUTH_TOKEN:=myToken}
 export ES_ENABLED=${ES_ENABLED:=True}
