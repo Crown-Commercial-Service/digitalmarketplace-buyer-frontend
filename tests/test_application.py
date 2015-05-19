50ed382ac7ab2173ae7a5d54e265ec74e01545f0
Chris Hill-Scott <me@quis.cc>
2015-05-19 11:25:42 +0100

    Add tests for redirection

--- tests/test_application.py
+++ tests/test_application.py
@@ -9,3 +9,11 @@ class TestApplication(BaseApplicationTest):
     def test_404(self):
         response = self.client.get('/not-found')
         assert 404 == response.status_code
+
+    def test_trailing_slashes(self):
+        response = self.client.get('')
+        assert 301 == response.status_code
+        assert "http://localhost/" == response.location
+        response = self.client.get('/trailing/')
+        assert 301 == response.status_code
+        assert "http://localhost/trailing" == response.location
