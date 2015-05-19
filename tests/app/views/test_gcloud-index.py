b6de2149f788453ba86ef8662561ca4734cc5988
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-05-19 21:19:14 +0100

    Update all links to use the new /g-cloud/ URLs
    - using url_for for all links
    - tests for links on g-cloud landing page
    - tests for form submission path on search page
    - tests for links to services on search results page

---
+++ tests/app/views/test_gcloud-index.py
@@ -0,0 +1,39 @@
+import mock
+from nose.tools import assert_equal, assert_true
+from ...helpers import BaseApplicationTest
+
+
+class TestGCloudIndexResults(BaseApplicationTest):
+    def setup(self):
+        super(TestGCloudIndexResults, self).setup()
+
+        self._search_api_client = mock.patch(
+            'app.main.views.search_api_client'
+        ).start()
+
+        self.search_results = self._get_search_results_fixture_data()
+
+    def teardown(self):
+        self._search_api_client.stop()
+
+    def test_renders_correct_search_links(self):
+        self._search_api_client.search_services.return_value = \
+            self.search_results
+
+        res = self.client.get('/g-cloud')
+        assert_equal(200, res.status_code)
+        assert_true(
+            'form action="/g-cloud/search'
+            in res.get_data(as_text=True))
+        assert_true(
+            '/g-cloud/search?lot=saas'
+            in res.get_data(as_text=True))
+        assert_true(
+            '/g-cloud/search?lot=scs'
+            in res.get_data(as_text=True))
+        assert_true(
+            '/g-cloud/search?lot=paas'
+            in res.get_data(as_text=True))
+        assert_true(
+            '/g-cloud/search?lot=iaas'
+            in res.get_data(as_text=True))
