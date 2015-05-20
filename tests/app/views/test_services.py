4c2c0cedadb3e7e1c42d7d2c0881424bfb1e4daa
Paul Craig <paul.craig@digital.cabinet-office.gov.uk>
2015-05-20 15:32:48 +0100

    Add supplier fixture, augment functional tests

--- tests/app/views/test_services.py
+++ tests/app/views/test_services.py
@@ -14,14 +14,34 @@ class TestServicePage(BaseApplicationTest):
         ).start()
 
         self.service = self._get_service_fixture_data()
+        self.supplier = self._get_supplier_fixture_data()
+
+        self._data_api_client.get_service.return_value = self.service
+        self._data_api_client.get_supplier.return_value = self.supplier
 
     def teardown(self):
         self._data_api_client.stop()
 
     def test_service_page_url(self):
-        self._data_api_client.get_service.return_value = \
-            self.service
 
-        res = self.client.get('/services/1234567890123456')
+        service_id = self.service['services']['id']
+
+        res = self.client.get('/services/{}'.format(service_id))
         assert_equal(200, res.status_code)
         assert_true("<h1>Blogging platform</h1>" in res.get_data(as_text=True))
+
+        supplier_name = self.supplier['suppliers']['name']
+        contact_info = self.supplier['suppliers']['contactInformation'][0]
+
+        assert_true(
+            "Contact {}".format(supplier_name) in res.get_data(as_text=True))
+
+        for contact_detail in [
+            contact_info['contactName'],
+            contact_info['phoneNumber'],
+            'mailto:{}'.format(contact_info['email'])
+        ]:
+
+            assert_true(
+                "{}".format(contact_detail)
+                in res.get_data(as_text=True))
