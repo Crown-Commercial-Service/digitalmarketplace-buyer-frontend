0f50ec37cd6e8662fbab7e90559467353a968666
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-05-27 08:10:15 +0100

    refactored for more clarity

--- app/helpers/search_helpers.py
+++ app/helpers/search_helpers.py
@@ -49,34 +49,43 @@ def total_pages(total, page_size):
 
 
 def pagination(num_services, page_size, page=None):
-    total = total_pages(num_services, page_size)
+    total_num_pages = total_pages(num_services, page_size)
     next_page = None
     prev_page = None
-
-    if page and num_services > page_size:
-        next_page = page + 1
-    elif not page and num_services > page_size:
-        next_page = 2
-
-    if page and page > 1:
-        prev_page = page - 1
-
-    if page and page > total:
-        prev_page = total
-
     show_prev = False
     show_next = False
 
-    if page and page > 1:
-        show_prev = True
+    # are we currently paginated?
+    if page:
+        # next page is page + 1 if num services exceeds page size
+        if num_services > page_size:
+            next_page = page + 1
 
-    if page and (total > 1 and page < total):
-        show_next = True
-    elif not page and total > 1:
-        show_next = True
+        # prev page is page - 1 OR last page if page beyond upper bound
+        if page > 1:
+            prev_page = page - 1
+        elif page > total_num_pages:
+            prev_page = total_num_pages
+
+        # show previous link if after page 1
+        if page > 1:
+            show_prev = True
+
+        # Show next link if have multiple pages and are not at last page
+        if total_num_pages > 1 and page < total_num_pages:
+            show_next = True
+    # on first page
+    else:
+        # next page always page 2 if have more services than page size
+        if num_services > page_size:
+            next_page = 2
+        # show next if have more than 1 page
+        # not on last page as no page param
+        if total_num_pages > 1:
+            show_next = True
 
     return {
-        "total_pages": total,
+        "total_pages": total_num_pages,
         "show_prev": show_prev,
         "show_next": show_next,
         "next_page": next_page,
