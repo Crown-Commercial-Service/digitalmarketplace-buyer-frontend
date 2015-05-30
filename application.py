280e9cb77559855cb7d6596398c6cdab62a74ca0
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-05-30 11:01:43 +0100

    Logging

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ port = int(os.environ.get('PORT', 5002))
 print "POST {}".format(port)
 application = create_app(os.getenv('DM_ENVIRONMENT') or 'development')
 manager = Manager(application)
-manager.add_command("runserver", Server(port=port))
+manager.add_command("runserver", Server(host='0.0.0.0', port=port))
 
 if __name__ == '__main__':
     manager.run()
