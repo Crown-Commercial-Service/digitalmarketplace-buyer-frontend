c7b3febdf4053a5588ad41fdec89b32f0429ab81
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-05-30 10:24:00 +0100

    Setting up heroku port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn application
+web: python application.py runserver
--- application.py
+++ application.py
@@ -4,9 +4,10 @@ import os
 from app import create_app
 from flask.ext.script import Manager, Server
 
+port = int(os.environ.get('PORT', 5002))
 application = create_app(os.getenv('DM_ENVIRONMENT') or 'development')
 manager = Manager(application)
-manager.add_command("runserver", Server(port=5002))
+manager.add_command("runserver", Server(port=port))
 
 if __name__ == '__main__':
     manager.run()
