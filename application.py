5c3195e84ff1349e05417c0d2f492d5b41836369
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-05-30 10:55:54 +0100

    Logging

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from app import create_app
 from flask.ext.script import Manager, Server
 
 port = int(os.environ.get('PORT', 5002))
-print port
+print "POST {}".format(port)
 application = create_app(os.getenv('DM_ENVIRONMENT') or 'development')
 manager = Manager(application)
 manager.add_command("runserver", Server(port=port))
