4b0c5479d812fb88de3d9f4ae3b478e2d1e69b48
Chris Hill-Scott <me@quis.cc>
2015-06-12 12:20:21 +0100

    Use option select from toolkit
    This updates the buyer app to
    - use option-select from the Digital Marketplace frontend toolkit
    - which, by proxy, uses the GOV.UK component version of option select,
      which this app previously did not use

--- app/assets/javascripts/_onready.js
+++ app/assets/javascripts/_onready.js
@@ -1,18 +1,10 @@
 (function (root) {
+
   var GOVUK = root.GOVUK || {};
-
-  if (GOVUK.CheckboxFilter) {
-    var filters = $('.js-openable-filter').map(function(){
-      return new GOVUK.CheckboxFilter({el:$(this)});
-    });
-
-    if (filters.length > 0 && $('.js-openable-filter').not('.closed').length == 0) {
-      filters[0].open();
-    }
-  }
 
   $('details').details();
   if (!$.fn.details.support) {
     $('html').addClass('no-details');
   }
+
 })(window);
