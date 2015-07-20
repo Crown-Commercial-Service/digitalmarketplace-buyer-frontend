732f098058f6ecbb5ffb5eba19ac4f52e9d2a55d
Martyn Inglis <martyn.inglis@digital.cabinet-office.gov.uk>
2015-07-20 10:31:55 +0100

    Fix cookie link on seen cookies message warning to use local cookies page
    - seen cookie message pointed to GOV.UK
    - changed to use /cookies

--- tests/app/views/test_application.py
+++ tests/app/views/test_application.py
@@ -12,3 +12,11 @@ class TestApplication(BaseApplicationTest):
         assert_true(
             'trackPageview'
             in res.get_data(as_text=True))
+
+    def test_should_use_local_cookie_page_on_cookie_message(self):
+        res = self.client.get('/')
+        assert_equal(200, res.status_code)
+        assert_true(
+            '<p>GOV.UK uses cookies to make the site simpler. <a href="/cookies">Find out more about cookies</a></p>'
+            in res.get_data(as_text=True)
+        )
