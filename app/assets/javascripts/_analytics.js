4e18ced31a7c680e9a046fea83e1ae70472e13ee
Tom Byers <tombaromba@gmail.com>
2015-07-29 16:43:04 +0100

    Bring in new version of analytics
    Only uses Universal now.
    
    Classic was included in the new API initially to
    support older implementations but was removed in
    https://github.com/alphagov/govuk_frontend_toolkit/pull/194.

--- app/assets/javascripts/_analytics.js
+++ app/assets/javascripts/_analytics.js
@@ -3,7 +3,7 @@
   GOVUK.Tracker.load();
   var cookieDomain = (document.domain === 'www.beta.digitalmarketplace.service.gov.uk') ? '.digitalmarketplace.service.gov.uk' : document.domain;
   var property = (document.domain === 'www.digitalmarketplace.service.gov.uk') ? 'UA-49258698-1' : 'UA-49258698-3';
-  GOVUK.analytics = new GOVUK.Tracker({
+  GOVUK.analytics = new GOVUK.Analytics({
     universalId: property,
     cookieDomain: cookieDomain
   });
