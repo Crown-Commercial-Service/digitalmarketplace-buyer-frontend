6a33c9727f35bc64a361c359c48d9507c626f056
Tom Byers <tombaromba@gmail.com>
2015-07-29 16:45:37 +0100

    Track www.digitalmarketplace.service.gov.uk now
    This code was tracking
    www.beta.digitalmarketplace.service.gov.uk which
    is now redundant.

--- app/assets/javascripts/_analytics.js
+++ app/assets/javascripts/_analytics.js
@@ -1,7 +1,7 @@
 (function() {
   "use strict";
   GOVUK.Tracker.load();
-  var cookieDomain = (document.domain === 'www.beta.digitalmarketplace.service.gov.uk') ? '.digitalmarketplace.service.gov.uk' : document.domain;
+  var cookieDomain = (document.domain === 'www.digitalmarketplace.service.gov.uk') ? '.digitalmarketplace.service.gov.uk' : document.domain;
   var property = (document.domain === 'www.digitalmarketplace.service.gov.uk') ? 'UA-49258698-1' : 'UA-49258698-3';
   GOVUK.analytics = new GOVUK.Analytics({
     universalId: property,
