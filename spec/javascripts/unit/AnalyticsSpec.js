013fa96c2ee080c0b3b1aa13816095cb4a4f69d8
Tom Byers <tombaromba@gmail.com>
2015-07-30 17:40:44 +0100

    Add JavaScript test for analytics
    The Digital Marketplace version of analytics sends
    an additional property to the `create` method of
    `window.ga` so this adds a test for that.

---
+++ spec/javascripts/unit/AnalyticsSpec.js
@@ -0,0 +1,27 @@
+describe("GOVUK.Analytics", function () {
+  var analytics;
+
+  beforeEach(function() {
+    window.ga = function() {};
+    spyOn(window, 'ga');
+    analytics = new GOVUK.Analytics({
+      universalId: 'universal-id',
+      cookieDomain: 'www.digitalmarketplace.service.gov.uk'
+    });
+  });
+
+  describe('when created', function() {
+    var universalSetupArguments;
+
+    beforeEach(function() {
+      universalSetupArguments = window.ga.calls.allArgs();
+    });
+
+    it('configures a universal tracker', function() {
+      expect(universalSetupArguments[0]).toEqual(['create', 'universal-id', {
+        'cookieDomain': 'www.digitalmarketplace.service.gov.uk',
+        'allowLinker': true
+      }]);
+    });
+  })
+});
