fd1fcc6f35f2e36290fe1143edcea2334e079267
Tom Byers <tombaromba@gmail.com>
2015-07-30 11:54:09 +0100

    Patch GOVUK.Analytics to add 'allowLinker' option
    The GOVUK.Analytics interface doesn't set the
    'allowLinker' option when creating a ga tracker.
    
    In order to preserve a vendor-agnostic
    interface it also doesn't allow this to be sent
    through when creating a new GOVUK.Analytics
    instance.
    
    This commit patches
    GOVUK.GoogleAnalyticsUniversalTracker to include
    'autoLinker' as a default option for all ga
    trackers.

--- app/assets/javascripts/_analytics.js
+++ app/assets/javascripts/_analytics.js
@@ -1,11 +1,35 @@
 (function() {
   "use strict";
-  GOVUK.Tracker.load();
+  GOVUK.Analytics.load();
   var cookieDomain = (document.domain === 'www.digitalmarketplace.service.gov.uk') ? '.digitalmarketplace.service.gov.uk' : document.domain;
   var property = (document.domain === 'www.digitalmarketplace.service.gov.uk') ? 'UA-49258698-1' : 'UA-49258698-3';
+
+  // wrapper function for calls to window.ga, used by all Google Analytics trackers
+  function sendToGa() {
+    if (typeof window.ga === "function") {
+      ga.apply(window, arguments);
+    }
+  }
+  // Rewritten universal tracker to allow setting of the autoLinker property
+  var DMGoogleAnalyticsUniversalTracker = function(id, cookieDomain) {
+    configureProfile(id, cookieDomain);
+    anonymizeIp();
+
+    function configureProfile(id, cookieDomain) {
+      sendToGa('create', id, {'cookieDomain': cookieDomain, 'allowLinker': true });
+    }
+
+    function anonymizeIp() {
+      // https://developers.google.com/analytics/devguides/collection/analyticsjs/advanced#anonymizeip
+      sendToGa('set', 'anonymizeIp', true);
+    }
+  };
+  DMGoogleAnalyticsUniversalTracker.prototype = GOVUK.GoogleAnalyticsUniversalTracker.prototype;
+  GOVUK.GoogleAnalyticsUniversalTracker = DMGoogleAnalyticsUniversalTracker;
   GOVUK.analytics = new GOVUK.Analytics({
     universalId: property,
     cookieDomain: cookieDomain
   });
   GOVUK.analytics.trackPageview();
+  GOVUK.analytics.addLinkedTrackerDomain(property, 'link', 'digitalservicesstore.service.gov.uk');
 })();
