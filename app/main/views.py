a3ce60394898e53db2d6ba467c8d353bfb1558f6
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2015-09-08 15:31:10 +0100

    Specify unicode when mixing in string URL parameters
    The `.format` method in Python 2 on a string expects ascii-encodable things, and breaks if asked to format non-ascii characters.
    
    By adding the `u` here `.format` is happy.

--- app/main/views.py
+++ app/main/views.py
@@ -128,7 +128,7 @@ def get_service_by_id(service_id):
     try:
         service = data_api_client.get_service(service_id)
         if service is None or service['services'].get('status') != 'published':
-            abort(404, "Service ID '{}' can not be found".format(service_id))
+            abort(404, u"Service ID '{}' can not be found".format(service_id))
 
         service_data = service['services']
         service_view_data = Service(
