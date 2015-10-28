23584d2acb98f2602a48f0ef075a5e3b079bc57e
Chris Hill-Scott <me@quis.cc>
2015-10-28 16:19:39 +0000

    Bring in 11.1.1 of utils To allow us to use the version of `init_manager` which returns a `Manager` instance.

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ from app import create_app
 from dmutils import init_manager
 
 application = create_app(os.getenv('DM_ENVIRONMENT') or 'development')
-init_manager(application, 5002, ['./app/content/frameworks'])
+manager = init_manager(application, 5002, ['./app/content/frameworks'])
 
 if __name__ == '__main__':
     manager.run()
