e217f5b9a96893d77a23628a89c35c7cd7c88045
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-01-22 15:38:56 +0000

    Add secrets to run_app script, to allow sessions to work when running locally

--- scripts/run_app.sh
+++ scripts/run_app.sh
@@ -16,6 +16,10 @@ export DM_SEARCH_API_URL=${DM_SEARCH_API_URL:=http://localhost:5001}
 export DM_SEARCH_API_AUTH_TOKEN=${DM_SEARCH_API_AUTH_TOKEN:=myToken}
 export ES_ENABLED=${ES_ENABLED:=True}
 
+export DM_MANDRILL_API_KEY=${DM_MANDRILL_API_KEY:=not_a_real_key}
+export DM_PASSWORD_SECRET_KEY=${DM_PASSWORD_SECRET_KEY:=verySecretKey}
+export DM_SHARED_EMAIL_KEY=${DM_SHARED_EMAIL_KEY:=very_secret}
+
 echo "Environment variables in use:" 
 env | grep DM_
 
