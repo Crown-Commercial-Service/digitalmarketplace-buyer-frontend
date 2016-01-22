86735c973a255f90b46ad1da2476f6e3b72e562b
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-01-22 14:59:00 +0000

    Add config for login session handling and CSRF protection

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,19 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, session, abort
+from flask_login import LoginManager
+from flask_wtf.csrf import CsrfProtect
 
 import dmapiclient
 from dmutils import init_app, flask_featureflags
 from dmutils.content_loader import ContentLoader
+from dmutils.user import User
 
 from config import configs
 
+login_manager = LoginManager()
 data_api_client = dmapiclient.DataAPIClient()
 search_api_client = dmapiclient.SearchAPIClient()
 feature_flags = flask_featureflags.FeatureFlag()
+csrf = CsrfProtect()
 
 content_loader = ContentLoader('app/content')
 content_loader.load_manifest('g-cloud-6', 'services', 'search_filters')
@@ -23,6 +28,7 @@ def create_app(config_name):
         configs[config_name],
         data_api_client=data_api_client,
         feature_flags=feature_flags,
+        login_manager=login_manager,
         search_api_client=search_api_client
     )
 
@@ -31,10 +37,28 @@ def create_app(config_name):
 
     application.register_blueprint(status_blueprint)
     application.register_blueprint(main_blueprint)
-
     main_blueprint.config = {
         'BASE_TEMPLATE_DATA': application.config['BASE_TEMPLATE_DATA'],
     }
+
+    login_manager.login_view = 'main.render_login'
+    login_manager.login_message_category = "must_login"
+    csrf.init_app(application)
+
+    @csrf.error_handler
+    def csrf_handler(reason):
+        if 'user_id' not in session:
+            application.logger.info(
+                u'csrf.session_expired: Redirecting user to log in page'
+            )
+
+            return application.login_manager.unauthorized()
+
+        application.logger.info(
+            u'csrf.invalid_token: Aborting request, user_id: {user_id}',
+            extra={'user_id': session['user_id']})
+
+        abort(400, reason)
 
     @application.before_request
     def remove_trailing_slash():
@@ -50,4 +74,14 @@ def create_app(config_name):
             else:
                 return redirect(request.path[:-1], code=301)
 
+    @application.before_request
+    def refresh_session():
+        session.permanent = True
+        session.modified = True
+
     return application
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.load_user(data_api_client, user_id)
--- config.py
+++ config.py
@@ -20,41 +20,57 @@ class Config(object):
     VERSION = get_version_label(
         os.path.abspath(os.path.dirname(__file__))
     )
-    DEBUG = False
-    ASSET_PATH = '/static/'
-    BASE_TEMPLATE_DATA = {
-        'header_class': 'with-proposition',
-        'asset_path': ASSET_PATH,
-        'asset_fingerprinter': AssetFingerprinter(asset_root=ASSET_PATH)
-    }
+    SESSION_COOKIE_NAME = 'dm_session'
+    SESSION_COOKIE_PATH = '/'
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = True
+
+    PERMANENT_SESSION_LIFETIME = 4*3600
+
+    WTF_CSRF_ENABLED = True
+    WTF_CSRF_TIME_LIMIT = None
+
     DM_DATA_API_URL = os.getenv('DM_API_URL')
     DM_DATA_API_AUTH_TOKEN = os.getenv('DM_BUYER_FRONTEND_API_AUTH_TOKEN')
     DM_SEARCH_API_URL = os.getenv('DM_SEARCH_API_URL')
     DM_SEARCH_API_AUTH_TOKEN = os.getenv(
         'DM_BUYER_FRONTEND_SEARCH_API_AUTH_TOKEN'
     )
+    DM_MANDRILL_API_KEY = None
+
     # matches api(s)
     DM_SEARCH_PAGE_SIZE = 100
 
     # This is just a placeholder
     ES_ENABLED = True
 
-    # Search API
-    DM_SEARCH_API_URL = "http://localhost:5001"
-    DM_SEARCH_API_AUTH_TOKEN = "myToken"
-    ES_ENABLED = True
+    DEBUG = False
 
-    DM_DATA_API_URL = "http://localhost:5000"
-    DM_DATA_API_AUTH_TOKEN = "myToken"
+    RESET_PASSWORD_EMAIL_NAME = 'Digital Marketplace Admin'
+    RESET_PASSWORD_EMAIL_FROM = 'enquiries@digitalmarketplace.service.gov.uk'
+    RESET_PASSWORD_EMAIL_SUBJECT = 'Reset your Digital Marketplace password'
+
+    CREATE_USER_SUBJECT = 'Create your Digital Marketplace account'
+    SECRET_KEY = os.getenv('DM_PASSWORD_SECRET_KEY')
+    SHARED_EMAIL_KEY = os.getenv('DM_SHARED_EMAIL_KEY')
+    RESET_PASSWORD_SALT = 'ResetPasswordSalt'
+    INVITE_EMAIL_SALT = 'InviteEmailSalt'
+
+    ASSET_PATH = '/static/'
+    BASE_TEMPLATE_DATA = {
+        'header_class': 'with-proposition',
+        'asset_path': ASSET_PATH,
+        'asset_fingerprinter': AssetFingerprinter(asset_root=ASSET_PATH)
+    }
+
+    # Feature Flags
+    RAISE_ERROR_ON_MISSING_FEATURES = True
 
     # LOGGING
     DM_LOG_LEVEL = 'DEBUG'
     DM_APP_NAME = 'buyer-frontend'
     DM_LOG_PATH = '/var/log/digitalmarketplace/application.log'
     DM_DOWNSTREAM_REQUEST_ID_HEADER = 'X-Amz-Cf-Id'
-
-    # Feature Flags
-    RAISE_ERROR_ON_MISSING_FEATURES = True
 
     @staticmethod
     def init_app(app):
@@ -69,6 +85,10 @@ class Config(object):
 class Test(Config):
     DEBUG = True
     DM_LOG_LEVEL = 'CRITICAL'
+    WTF_CSRF_ENABLED = False
+    DM_MANDRILL_API_KEY = 'MANDRILL'
+    SHARED_EMAIL_KEY = "KEY"
+    SECRET_KEY = "KEY"
 
 
 class Development(Config):
