fc9616cdcd39fd62f2470a88edd03f26d70fb081
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2016-02-10 17:26:31 +0000

    Add first version of POST endpoint to create a brief

--- app/buyers/views/buyers.py
+++ app/buyers/views/buyers.py
@@ -1,9 +1,11 @@
-from flask import abort, render_template
+from flask import abort, render_template, request
 from flask_login import current_user
 
 from app import data_api_client
 from .. import buyers, content_loader
 from ...helpers.search_helpers import get_template_data
+
+from dmapiclient import HTTPError
 
 
 @buyers.route('/buyers')
@@ -49,3 +51,38 @@ def start_new_brief(framework_slug, lot_slug):
         section=section,
         **dict(buyers.config['BASE_TEMPLATE_DATA'])
     ), 200
+
+
+@buyers.route('/buyers/frameworks/<framework_slug>/requirements/<lot_slug>/create', methods=['POST'])
+def create_new_brief(framework_slug, lot_slug):
+    framework = data_api_client.get_framework(framework_slug)["frameworks"]
+    if framework["status"] != "live":
+        abort(404)
+
+    try:
+        lot = next(lot for lot in framework['lots'] if lot['slug'] == lot_slug)
+    except StopIteration:
+        abort(404)
+    if not lot['allowsBrief']:
+        abort(404)
+
+    content = content_loader.get_manifest(framework_slug, 'edit_brief').filter(
+        {'lot': lot['slug']}
+    )
+
+    section = content.get_section(content.get_next_editable_section_id())
+
+    update_data = section.get_data(request.form)
+
+    try:
+        brief = data_api_client.create_brief(
+            framework_slug,
+            lot_slug,
+            current_user.id,
+            update_data,
+            updated_by=current_user.email_address,
+            page_questions=update_data.keys()
+        )["briefs"]
+    except HTTPError as e:
+        print("API ERROR ERROR ERROR: {}".format(e))
+        raise e
