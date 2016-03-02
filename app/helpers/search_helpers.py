b96e7a8580395a6d809c6e11ad2351f6a66a4bd2
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-03-02 10:19:54 +0000

    Remove get_template_data method
    This method mutates the base template data of the blueprint, which can lead to problems.  Better kill it with fire.

--- app/helpers/search_helpers.py
+++ app/helpers/search_helpers.py
@@ -6,16 +6,6 @@ from werkzeug.datastructures import MultiDict
 from dmutils.formats import lot_to_lot_case
 
 
-def get_template_data(blueprint, view_data):
-    """Returns a single object holding the base template data with the view
-       data added"""
-
-    template_data = blueprint.config['BASE_TEMPLATE_DATA']
-    for key in view_data:
-        template_data[key] = view_data[key]
-    return template_data
-
-
 def get_lot_from_request(request):
     return request.args.get('lot', None)
 
