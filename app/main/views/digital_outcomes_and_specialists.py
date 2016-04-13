09bc73de0b909b8a4cb1e39dfb1604f6ae959446
Kev Keenoy <kevin.keenoy@digital.cabinet-office.gov.uk>
2016-04-13 16:04:59 +0100

    Add start page for labs
    This adds a new page, linked to from the home page, where users can read instructions for how to hire labs and download the CSV file of user labs.

--- app/main/views/digital_outcomes_and_specialists.py
+++ app/main/views/digital_outcomes_and_specialists.py
@@ -2,20 +2,28 @@
 from __future__ import unicode_literals
 from app import data_api_client
 
-from flask import render_template
-from ...helpers.buyers_helpers import get_framework_and_lot, count_suppliers_on_lot
+from flask import abort, render_template
+from ...helpers.buyers_helpers import get_framework_and_lot
 from ...main import main
+
+
+@main.route('/buyers/frameworks/<framework_slug>/requirements/user-research-studios', methods=['GET'])
+def studios_start_page(framework_slug):
+    framework = data_api_client.get_framework(framework_slug)['frameworks']
+    if framework['status'] != 'live':
+        abort(404)
+
+    return render_template(
+        "buyers/studios_start_page.html"
+    ), 200
 
 
 @main.route('/buyers/frameworks/<framework_slug>/requirements/<lot_slug>', methods=['GET'])
 def info_page_for_starting_a_brief(framework_slug, lot_slug):
     framework, lot = get_framework_and_lot(framework_slug, lot_slug, data_api_client,
                                            status='live', must_allow_brief=True)
-    print("LOT: {}".format(lot))
-    print("FRAMEWORK: {}".format(framework))
     return render_template(
         "buyers/start_brief_info.html",
         framework=framework,
-        lot=lot,
-        supplier_count=count_suppliers_on_lot(framework, lot)
+        lot=lot
     ), 200
---
+++ tests/app/views/test_digital_outcomes_and_specialists.py
@@ -0,0 +1,87 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from ...helpers import BaseApplicationTest
+from dmapiclient import api_stubs
+import mock
+from lxml import html
+
+
+@mock.patch('app.main.views.digital_outcomes_and_specialists.data_api_client')
+class TestStartBriefInfoPage(BaseApplicationTest):
+    def test_show_start_brief_info_page(self, data_api_client):
+        with self.app.app_context():
+            data_api_client.get_framework.return_value = api_stubs.framework(
+                slug='digital-outcomes-and-specialists',
+                status='live',
+                lots=[
+                    api_stubs.lot(slug='digital-specialists', allows_brief=True),
+                ]
+            )
+
+            res = self.client.get(
+                "/buyers/frameworks/digital-outcomes-and-specialists/requirements/digital-specialists")
+            assert res.status_code == 200
+            document = html.fromstring(res.get_data(as_text=True))
+            assert document.xpath('//h1')[0].text_content().strip() == "Find an individual specialist"
+
+    def test_404_if_lot_does_not_allow_brief(self, data_api_client):
+        with self.app.app_context():
+            data_api_client.get_framework.return_value = api_stubs.framework(
+                slug='digital-outcomes-and-specialists',
+                status='live',
+                lots=[
+                    api_stubs.lot(slug='digital-specialists', allows_brief=False)
+                ]
+            )
+
+            res = self.client.get(
+                "/buyers/frameworks/digital-outcomes-and-specialists/requirements/digital-specialists")
+            assert res.status_code == 404
+
+    def test_404_if_framework_status_is_not_live(self, data_api_client):
+        with self.app.app_context():
+            data_api_client.get_framework.return_value = api_stubs.framework(
+                slug='digital-outcomes-and-specialists',
+                status='open',
+                lots=[
+                    api_stubs.lot(slug='digital-specialists', allows_brief=True),
+                ]
+            )
+
+            res = self.client.get(
+                "/buyers/frameworks/digital-outcomes-and-specialists/requirements/digital-specialists")
+            assert res.status_code == 404
+
+
+@mock.patch('app.main.views.digital_outcomes_and_specialists.data_api_client')
+class TestStartStudiosInfoPage(BaseApplicationTest):
+    def test_show_start_studios_info_page(self, data_api_client):
+        with self.app.app_context():
+            data_api_client.get_framework.return_value = api_stubs.framework(
+                slug='digital-outcomes-and-specialists',
+                status='live',
+                lots=[
+                    api_stubs.lot(slug='user-research-studios'),
+                ]
+            )
+
+            res = self.client.get(
+                "/buyers/frameworks/digital-outcomes-and-specialists/requirements/user-research-studios")
+            assert res.status_code == 200
+            document = html.fromstring(res.get_data(as_text=True))
+            assert document.xpath('//h1')[0].text_content().strip() == "Find a user research lab"
+
+    def test_404_if_framework_status_is_not_live(self, data_api_client):
+        with self.app.app_context():
+            data_api_client.get_framework.return_value = api_stubs.framework(
+                slug='digital-outcomes-and-specialists',
+                status='open',
+                lots=[
+                    api_stubs.lot(slug='user-research-studios'),
+                ]
+            )
+
+            res = self.client.get(
+                "/buyers/frameworks/digital-outcomes-and-specialists/requirements/user-research-studios")
+            assert res.status_code == 404
