955e5cc44e28bf1ed730dd9f689a2175a984da40
Tom Byers <tombaromba@gmail.com>
2016-05-27 15:34:48 +0100

    Make tests work with new analytics structure
    Includes:
    
    - move sortCalls method to top of analytics test
    - add files to JS test manifest
    - fix the arguments we're testing when setting up
      a tracker
    - fix tests for cross-domain tracking

--- spec/javascripts/manifest.js
+++ spec/javascripts/manifest.js
@@ -1,9 +1,14 @@
 // files are loaded from the /spec/javascripts/support folder so paths are relative to that
 var manifest = {
   support : [
+    '../../../bower_components/jquery/dist/jquery.js',
     '../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/google-analytics-universal-tracker.js',
     '../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/analytics.js',
-    '../../../app/assets/javascripts/analytics/_dm-analytics.js'
+    '../../../app/assets/javascripts/analytics/_dm-analytics.js',
+    '../../../app/assets/javascripts/analytics/_register.js',
+    '../../../app/assets/javascripts/analytics/_pageViews.js',
+    '../../../app/assets/javascripts/analytics/_events.js',
+    '../../../app/assets/javascripts/analytics/_init.js'
   ],
   test : [
     '../unit/AnalyticsSpec.js'
--- spec/javascripts/unit/AnalyticsSpec.js
+++ spec/javascripts/unit/AnalyticsSpec.js
@@ -1,5 +1,29 @@
 describe("GOVUK.Analytics", function () {
-  var analytics;
+  var analytics,
+      sortCalls;
+      
+  SortCallsToGaByMethod = function (calls) {
+    var gaMethodCalls = {},
+        callNum = calls.length;
+
+    while (callNum--) {
+      var method = calls[callNum].args.shift(),
+          args = calls[callNum].args;
+
+      if (gaMethodCalls.hasOwnProperty(method)) {
+        gaMethodCalls[method].push(args);
+      } else {
+        gaMethodCalls[method] = [args];
+      }
+    }
+    this._calls = gaMethodCalls;
+  };
+  SortCallsToGaByMethod.prototype.callsTo = function (method) {
+    if (this._calls.hasOwnProperty(method)) {
+      return this._calls[method];
+    }
+    return [];
+  }; 
 
   beforeEach(function () {
     window.ga = function() {};
@@ -30,37 +54,22 @@ describe("GOVUK.Analytics", function () {
     });
 
     it('configures a universal tracker', function() {
-      var trackerId = 'UA-49258698-1';
-      expect(universalSetupArguments[0]).toEqual(['create', trackerId, {
-        'cookieDomain': document.domain
+      expect(universalSetupArguments[0]).toEqual(['create', 'UA-49258698-1', {
+        'cookieDomain': document.domain,
+        'allowLinker': true
       }]);
+    });
+
+    it('sets up cross-domain tracking', function () {
+      var gaMethodCalls = new SortCallsToGaByMethod(window.ga.calls.all());
+      // require linker plugin
+      expect(gaMethodCalls.callsTo('require')[0]).toEqual(['linker']);
+      // autolink the second domain
+      expect(gaMethodCalls.callsTo('linker:autoLink')[0]).toEqual([['digitalservicesstore.service.gov.uk']]);
     });
   });
 
   describe('when setting up cross-domain tracking', function () {
-    var sortCalls = function (calls) {
-      var gaMethodCalls = {},
-          callNum = calls.length;
-
-      while (callNum--) {
-        var method = calls[callNum].args.shift(),
-            args = calls[callNum].args;
-
-        if (gaMethodCalls.hasOwnProperty(method)) {
-          gaMethodCalls[method].push(args);
-        } else {
-          gaMethodCalls[method] = [args];
-        }
-      }
-      this._calls = gaMethodCalls;
-    };
-    sortCalls.prototype.callsTo = function (method) {
-      if (this._calls.hasOwnProperty(method)) {
-        return this._calls[method];
-      }
-      return [];
-    }; 
-
     beforeEach(function() {
       window.ga.calls.reset();
       analytics = new window.GOVUK.Analytics({
@@ -73,17 +82,8 @@ describe("GOVUK.Analytics", function () {
     });
 
     it('only sets up one tracker', function () {
-      var gaMethodCalls = new sortCalls(window.ga.calls.all());
-      expect(window.ga.calls.count()).toEqual(5);
-      // create tracker
-      expect(gaMethodCalls.callsTo('create').length).toEqual(1);
-      expect(gaMethodCalls.callsTo('create')[0]).toEqual(['universal-id', { 'cookieDomain': 'www.digitalmarketplace.service.gov.uk', 'allowLinker': true }]);
-      // anonymize IP
-      expect(gaMethodCalls.callsTo('set').length).toEqual(1);
-      expect(gaMethodCalls.callsTo('set')[0]).toEqual(['anonymizeIp', true]);
-      // send pageview
-      expect(gaMethodCalls.callsTo('send').length).toEqual(1);
-      expect(gaMethodCalls.callsTo('send')[0]).toEqual(['pageview']);
+      var gaMethodCalls = new SortCallsToGaByMethod(window.ga.calls.all());
+
       // require linker plugin
       expect(gaMethodCalls.callsTo('require').length).toEqual(1);
       expect(gaMethodCalls.callsTo('require')[0]).toEqual(['linker']);
