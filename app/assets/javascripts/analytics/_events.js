f57b5cc731d3392457b4f545c0e88f504c6872b9
Tom Byers <tombaromba@gmail.com>
2016-05-31 16:12:32 +0100

    Add DOS CSV list tracking
    Includes some refactoring of suppliers app
    download code to allow these requirements to use
    the existing code.
    
    Also includes:
    
    - wrapping of calls to
      window.location to allow tests to mock these calls
      more easily
    - includes some changes to allow required format of
      labels to be sent to ga events.
    - fixes for bits of the download event JS
    - bump govuk toolkit to 4.12.0 to bring in changes
      to the Google universal analytics code which
      allow you to specify the transport method
    
      See: https://github.com/alphagov/govuk_frontend_toolkit/commit/6a31ff274357e94847c2b0e950d6d352ae01ab65#diff-8b93af583c1a7ca5e9c48a89ebce2b38R42

--- app/assets/javascripts/analytics/_events.js
+++ app/assets/javascripts/analytics/_events.js
@@ -1,36 +1,112 @@
 (function (root, GOVUK) {
   "use strict";
 
-  GOVUK.GDM.analytics.events = {
-    'registerLinkClick': function (e) {
-      var $target = $(e.target),
-          href = $target.prop('href'),
-          category = 'internal-link',
-          action = $target.text(),
-          fileTypesRegExp = /\.(pdf|pda|odt|ods|odp|zip)$/,
-          currentHost = root.location.hostname,
-          currentHostRegExp = (currentHost !== '') ? new RegExp(currentHost) : /^$/g; // this ccode can run in an environment without a host, ie. a html file
+  // wrapper around access to window.location
+  GOVUK.GDM.analytics.location = {
+    'hostname': function () {
+      return root.location.hostname;
+    },
+    'pathname': function () {
+      return root.location.pathname;
+    },
+    'protocol': function () {
+      return root.location.protocol;
+    }
+  };
 
-      /*
-         File type matching based on those in:
-         https://github.com/alphagov/digitalmarketplace-utils/blob/8251d45f47593bd73c5e3b993e1734b5ee505b4b/dmutils/documents.py#L105
-      */
+  var LinkClick = function (e) {
+      var $target = $(e.target);
+
+      this.text = $target.text(),
+      this.href = $target.prop('href');
 
       // if the node clicked wasn't the link but a child of it
       if ($target[0].nodeName.toLowerCase() !== 'a') {
         $target = $target.closest('a');
-        href = $target.prop('href');
+        this.href = $target.prop('href');
       }
-      if (href.match(fileTypesRegExp) !== null) { // download link
+  };
+  
+  LinkClick.prototype.category = function () {
+      var category = 'internal-link';
+
+      if (this.fileType() !== null) { // download link
         category = 'download';
       }
-      else if ((href.match(/^(https|http){1}/) !== null) && (href.match(currentHostRegExp) === null)) {
+      else if ($.inArray(GOVUK.GDM.analytics.location.protocol(), ['http:', 'https:']) && !this.isOnHostDomain(this.href)) {
         category = 'external-link';
       }
-      GOVUK.analytics.trackEvent(category, action);
+      return category;
+  };
+  LinkClick.prototype.isOnHostDomain = function (url) {
+      var currentHost = GOVUK.GDM.analytics.location.hostname(),
+          currentHostRegExp = (currentHost !== '') ? new RegExp(currentHost) : /^$/g;
+
+      return url.match(currentHostRegExp) !== null;
+  };
+  LinkClick.prototype.fileType = function () {
+      /*
+         File type matching based on those in:
+         https://github.com/alphagov/digitalmarketplace-utils/blob/8251d45f47593bd73c5e3b993e1734b5ee505b4b/dmutils/documents.py#L105
+
+         CSV added to support suppliers download
+      */
+      var match = this.href.match(/\.(pdf|pda|odt|ods|odp|zip|csv)$/);
+      if (match !== null) { 
+        return match[1];
+      }
+      return false;
+  };
+  
+  var downloadLinkLabel = function (linkClick) {
+    var path = GOVUK.GDM.analytics.location.pathname().match(/\/buyers\/frameworks\/([a-z\-]+)\/requirements\/([a-z\-]+)\/*(\d+)*/),
+        lot = path[2],
+        lots = {
+          'digital-specialists': 'specialists',
+          'digital-outcomes': 'outcomes',
+          'user-research-participants': 'user research participants',
+          'user-research-studios': 'user research studios'
+        },
+        briefId;
+
+    if (linkClick.text.match('Download supplier responses') !== null) {
+        briefId = path[3];
+        return 'supplier response list' + ' | ' + lots[lot] + ' | ' + briefId;
+    } else {
+        switch (lot) {
+          case 'digital-specialists':
+            return 'list of specialists suppliers';
+            break;
+          case 'digital-outcomes':
+            return 'list of outcomes suppliers';
+            break;
+          case 'user-research-participants':
+            return 'list of user research participant suppliers';
+            break;
+          case 'user-research-studios':
+            return 'list of user research labs';
+            break;
+          default:
+            return false;
+        }
+    } 
+  };
+
+  GOVUK.GDM.analytics.LinkClick = LinkClick;
+
+  GOVUK.GDM.analytics.events = {
+    'supplierListDownload': function (e) {
+      var linkClick = new LinkClick(e);
+
+      if ((linkClick.category() === 'download') && (linkClick.fileType() === 'csv')) {
+        GOVUK.analytics.trackEvent('download', 'csv', {
+          'label': downloadLinkLabel(linkClick),
+          'transport': 'beacon'
+        });
+      }
     },
     'init': function () {
-      $('body').on('click', 'a', this.registerLinkClick)
+      $('body').on('click', 'a', this.supplierListDownload)
     }
   };
 })(window, window.GOVUK);
