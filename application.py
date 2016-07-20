12e26d3530ee8687e1996c5ea11f6106f433e404
Prakash Timilsina <prakash@timilsina.com>
2016-07-20 10:47:36 +1000

    added context processor in application to make current_user available on templates

--- application.py
+++ application.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
+from flask_login import current_user
 
 import os
 import sys
 from app import create_app
 from dmutils import init_manager
-
+from context_processors import user_context
 
 port = int(os.getenv('PORT', '5002'))
 application = create_app(os.getenv('DM_ENVIRONMENT') or 'development')
 manager = init_manager(application, port, ['./app/content/frameworks'])
 
 application.logger.info('Command line: {}'.format(sys.argv))
+application.context_processor(user_context)
 
 if __name__ == '__main__':
     try:
@@ -18,3 +20,4 @@ if __name__ == '__main__':
         manager.run()
     finally:
         application.logger.info('Manager finished')
+
---
+++ context_processors.py
@@ -0,0 +1,8 @@
+from flask_login import current_user
+
+def user_context():
+    """
+    Makes user data available throughout the template with variable current_user
+    example- {% if current_user.is_authenticated() %} {{ current_user.email_address }} {% endif %}
+    """
+    return {'current_user': current_user}
