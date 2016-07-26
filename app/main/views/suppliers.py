cfd2da361e9238e78663616fbf202f582b48d416
Alexey Bezhan <al@fueledbylemons.com>
2016-07-26 03:04:02 +0100

    Use vendored api_client instead of dmapiclient in /suppliers/<code>

--- app/main/views/suppliers.py
+++ app/main/views/suppliers.py
@@ -1,16 +1,10 @@
 # coding=utf-8
-from string import ascii_uppercase
-from app.main import main
-from flask import render_template, request, abort
-from app import data_api_client
-from dmapiclient import APIError
-from ...helpers.shared_helpers import parse_link
 import re
 
-try:
-    from urlparse import urlparse, parse_qs
-except ImportError:
-    from urllib.parse import urlparse, parse_qs
+from flask import render_template
+
+from app.main import main
+from app.api_client.data import DataAPIClient
 
 
 def process_prefix(prefix=None, format='view'):
@@ -31,7 +25,7 @@ def is_alpha(character):
 
 @main.route('/suppliers/<int:code>')
 def get_supplier(code):
-    supplier = data_api_client.get_supplier(code)['supplier']
+    supplier = DataAPIClient().get_supplier(code)['supplier']
 
     supplier_categories = set(
         price['serviceRole']['role'].replace('Junior', '').replace('Senior', '')
