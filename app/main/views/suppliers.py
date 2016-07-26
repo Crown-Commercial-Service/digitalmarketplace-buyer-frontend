a9c57885d9738c7a900cd70f33739fa07e7c959b
Alexey Bezhan <al@fueledbylemons.com>
2016-07-26 02:33:46 +0100

    Remove g-cloud suppliers views

--- app/main/views/suppliers.py
+++ app/main/views/suppliers.py
@@ -1,10 +1,16 @@
 # coding=utf-8
+from string import ascii_uppercase
+from app.main import main
+from flask import render_template, request, abort
+from app import data_api_client
+from dmapiclient import APIError
+from ...helpers.shared_helpers import parse_link
 import re
 
-from flask import render_template
-
-from app.main import main
-from app.api_client.data import DataAPIClient
+try:
+    from urlparse import urlparse, parse_qs
+except ImportError:
+    from urllib.parse import urlparse, parse_qs
 
 
 def process_prefix(prefix=None, format='view'):
@@ -25,7 +31,7 @@ def is_alpha(character):
 
 @main.route('/suppliers/<int:code>')
 def get_supplier(code):
-    supplier = DataAPIClient().get_supplier(code)['supplier']
+    supplier = data_api_client.get_supplier(code)['supplier']
 
     supplier_categories = set(
         price['serviceRole']['role'].replace('Junior', '').replace('Senior', '')
