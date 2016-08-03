e829971cc26077f875bc38494722cf4d88bd1c09
sarneaud <sarneaud@users.noreply.github.com>
2016-08-03 15:29:23 +1000

    Removed is_authenticated_workaround
    This was just for upgrading across the Flask Login API breaking change.

--- app/helpers/login_helpers.py
+++ app/helpers/login_helpers.py
@@ -2,18 +2,9 @@ from flask_login import current_user
 from flask import current_app, redirect, render_template, Response, url_for
 
 
-def is_authenticated_workaround(user):
-    # A breaking API change was made in Flask Login that makes is_authenticated into a property instead of a method.
-    # FIXME: migrate dmutils to the new API and remove this workaround
-    v = user.is_authenticated
-    if type(v) is bool:
-        return v
-    return v()
-
-
 def redirect_logged_in_user(next_url=None):
     site_url_prefix = current_app.config['URL_PREFIX']
-    if is_authenticated_workaround(current_user):
+    if current_user.is_authenticated:
         if next_url and next_url.startswith(site_url_prefix):
             return redirect(next_url)
         else:
