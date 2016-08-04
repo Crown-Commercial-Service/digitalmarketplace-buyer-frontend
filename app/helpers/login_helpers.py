4eec6104db67569654af9276f99f16d74a292a2e
sarneaud <sarneaud@users.noreply.github.com>
2016-08-04 14:24:23 +1000

    Add CSRF protection to brief handling code
    Add CSRF protection testing to test framework, too.

--- app/helpers/login_helpers.py
+++ app/helpers/login_helpers.py
@@ -1,5 +1,5 @@
 from flask_login import current_user
-from flask import current_app, redirect, render_template, Response, url_for
+from flask import current_app, redirect, url_for
 
 
 def redirect_logged_in_user(next_url=None):
@@ -11,16 +11,3 @@ def redirect_logged_in_user(next_url=None):
             return redirect(url_for('.supplier_search'))
 
     return redirect(url_for('.index'))
-
-
-def render_template_with_csrf(template_name, status_code=200, **kwargs):
-    response = Response(render_template(template_name, **kwargs))
-
-    # CSRF tokens are user-specific, even if the user isn't logged in
-    response.cache_control.private = True
-
-    max_age = current_app.config['DM_DEFAULT_CACHE_MAX_AGE']
-    max_age = min(max_age, current_app.config.get('CSRF_TIME_LIMIT', max_age))
-    response.cache_control.max_age = max_age
-
-    return response, status_code
