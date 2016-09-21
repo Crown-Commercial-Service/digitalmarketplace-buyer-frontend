d9a9712cfde7ceeeecb1af084da3b76ff2108008
sarneaud <sarneaud@users.noreply.github.com>
2016-09-21 15:00:52 +1000

    Remove some dead code from home page

--- app/main/views/marketplace.py
+++ app/main/views/marketplace.py
@@ -12,7 +12,6 @@ from dmcontent.content_loader import ContentNotFoundError
 from app.main.utils import get_page_list
 
 from ...main import main
-from ...helpers.shared_helpers import get_one_framework_by_status_in_order_of_preference, parse_link
 
 from ..forms.brief_forms import BriefSearchForm
 
@@ -22,33 +21,6 @@ from flask_weasyprint import HTML, render_pdf
 
 @main.route('/')
 def index():
-    temporary_message = {}
-
-    try:
-        frameworks = data_api_client.find_frameworks().get('frameworks')
-        framework = get_one_framework_by_status_in_order_of_preference(
-            frameworks,
-            ['open', 'coming', 'pending']
-        )
-
-        if framework is not None:
-            content_loader.load_messages(framework.get('slug'), ['homepage-sidebar'])
-            temporary_message = content_loader.get_message(
-                framework.get('slug'),
-                'homepage-sidebar',
-                framework.get('status')
-            )
-
-    # if there is a problem with the API we should still show the home page
-    except APIError:
-        frameworks = []
-    # if no message file is found (should never happen), throw a 500
-    except ContentNotFoundError:
-        current_app.logger.error(
-            "contentloader.fail No message file found for framework. "
-            "framework {} status {}".format(framework.get('slug'), framework.get('status')))
-        abort(500)
-
     try:
         buyers_count = data_api_client.get_buyers_count({'account_type': 'buyer'})['buyers']['total']
 
@@ -57,8 +29,7 @@ def index():
         briefs_count_json = data_api_client.get_briefs_count()
         briefs_count = briefs_count_json['briefs']['open_to_all'] + briefs_count_json['briefs']['open_to_one'] + \
             briefs_count_json['briefs']['open_to_selected']
-
-    except Exception, e:
+    except Exception as e:
         buyers_count = 0
         suppliers_count = 0
         briefs_count = 0
@@ -66,8 +37,6 @@ def index():
 
     return render_template(
         'index.html',
-        frameworks={framework['slug']: framework for framework in frameworks},
-        temporary_message=temporary_message,
         buyers_count=buyers_count,
         suppliers_count=suppliers_count,
         briefs_count=briefs_count
