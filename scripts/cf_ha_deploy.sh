41b473055904f841bd307bece547500471379f6a
sarneaud <sarneaud@users.noreply.github.com>
2016-09-22 13:46:00 +1000

    Stop staging downtime on push
    The cffi library seems to be needed for new pushes to work properly.
    Pushes to existing apps seem to have worked because the containers
    already had the library installed from earlier versions of the
    application.

--- scripts/cf_ha_deploy.sh
+++ scripts/cf_ha_deploy.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-cf unmap-route "$APP" service.gov.au --hostname marketplace
-cf push "$APP" -f service.gov.au-manifest.production.yml
-cf map-route "$APP" service.gov.au --hostname marketplace
+cf unmap-route "$APP" "$DOMAIN" --hostname "$HOSTNAME"
+cf push "$APP" -f "$MANIFEST"
+cf map-route "$APP" "$DOMAIN" --hostname "$HOSTNAME"
