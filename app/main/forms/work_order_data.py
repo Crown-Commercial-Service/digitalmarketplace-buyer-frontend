1fc041deb67247b2f038053a99068b071622bdec
matt-sm <mattysmith@gmail.com>
2016-09-29 09:29:17 +1000

    edit work order

---
+++ app/main/forms/work_order_data.py
@@ -0,0 +1,113 @@
+questions = {
+    'number': {
+        'heading': 'Work Order number',
+        'text': 'Add a work order number',
+        'summary': 'This is a seller-supplied work order number',
+        'label': 'Work Order number',
+        'message': 'You must provide a Work Order number',
+        'type': 'text',
+    },
+    'son': {
+        'heading': 'SON',
+        'text': 'Add an SON',
+        'summary': 'This is the SON',
+        'label': 'SON',
+        'message': 'You must provide an SON',
+        'type': 'text',
+    },
+    'orderPeriod': {
+        'heading': 'Order period',
+        'text': 'Add an order period',
+        'summary': 'This is the order period',
+        'label': 'Order period',
+        'message': 'You must provide an order period',
+        'type': 'text',
+    },
+    'deliverables': {
+        'heading': 'Services and deliverables',
+        'text': 'Add services and deliverables',
+        'summary': 'This is the order period',
+        'label': 'Services and deliverables',
+        'message': 'You must provide an order period'
+    },
+    'personnel': {
+        'heading': 'Specified personnel',
+        'text': 'Add specified personnel',
+        'summary': 'This is the specified personnel',
+        'label': 'Specified personnel',
+        'message': 'You must provide specified personnel'
+    },
+    'performanceCriteria': {
+        'heading': 'Performance criteria',
+        'text': 'Add performance criteria',
+        'summary': 'This is the performance criteria',
+        'label': 'Performance criteria',
+        'message': 'You must provide performance criteria'
+    },
+    'serviceCharges': {
+        'heading': 'Quoted service charges',
+        'text': 'Add service charges',
+        'summary': 'This is the service charges',
+        'label': 'Quoted service charges',
+        'message': 'You must provide service charges'
+    },
+    'expenses': {
+        'heading': 'Expenses',
+        'text': 'Add additional expenses',
+        'summary': 'This is the additional expenses',
+        'label': 'Expenses',
+        'message': 'You must provide additional expenses'
+    },
+    'agencyProperty': {
+        'heading': 'Agency intellectual property',
+        'text': 'Add agency intellectual property details',
+        'summary': 'This is the Agency intellectual property',
+        'label': 'Agency intellectual property',
+        'message': 'You must provide agency intellectual property details'
+    },
+    'sellerProperty': {
+        'heading': 'Seller intellectual property',
+        'text': 'Add seller intellectual property details',
+        'summary': 'This is the Seller intellectual property',
+        'label': 'Seller intellectual property',
+        'message': 'You must provide seller intellectual property details'
+    },
+    'securityClearance': {
+        'heading': 'Security requirements',
+        'text': 'Add security requirements details',
+        'summary': 'This is the security requirements',
+        'label': 'Security requirements',
+        'message': 'You must provide security requirements details'
+    },
+    'additionalTerms': {
+        'heading': 'Additional terms and conditions',
+        'text': 'Add additional terms and conditions',
+        'summary': 'This is the Additional terms and conditions',
+        'label': 'Additional terms and conditions',
+        'message': 'You must provide Additional terms and conditions'
+    },
+    'agency': {
+        'heading': 'Agency',
+        'text': 'Add agency',
+        'summary': 'Agency summary',
+        'abn': 'Abn',
+        'abnMessage': 'You must provide an abn',
+        'name': 'Agency Name',
+        'nameMessage': 'You must provide an agency name',
+        'contact': 'Agency Representative',
+        'contactMessage': 'You must provide an agency representative',
+        'type': 'address'
+    },
+    'seller': {
+        'heading': 'You',
+        'text': 'Add company',
+        'summary': 'Company summary',
+        'abn': 'Abn',
+        'abnMessage': 'You must provide an abn',
+        'name': 'Company Name',
+        'nameMessage': 'You must provide an company name',
+        'contact': 'Company Representative',
+        'contactMessage': 'You must provide an company representative',
+        'type': 'address'
+    },
+}
