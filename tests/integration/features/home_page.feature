acb20d8d7b9a139dce7c5339b9dfa6840ece6074
matt-sm <matt-sm@users.noreply.github.com>
2016-10-26 08:54:47 +1100

    Fix integration tests (#377)
    * wip
    
    * various changes to integration tests
    
    * pep8

--- tests/integration/features/home_page.feature
+++ tests/integration/features/home_page.feature
@@ -5,17 +5,17 @@ Scenario: Anonymous users can click Dashboard opportunities
     Given I am an anonymous user
     And I am on the / page
     When I click the View the latest briefs text
-    Then I should see the Seller opportunities – Digital Service Professionals – Digital Marketplace page
+    Then I should see the Latest opportunities page
 
 Scenario: Anonymous users can click Dashboard sellers
     Given I am an anonymous user
     And I am on the / page
     When I click the Browse the catalogue text
-    Then I should see the Seller search – Digital Marketplace page
+    Then I should see the Seller catalogue page
 
 Scenario: Anonymous users can click Dashboard buyers
     Given I am an anonymous user
     And I am on the / page
     When I click the Create a buyer account text
-    Then I should see the Request authorisation for a buyer account – Digital Marketplace page
+    Then I should see the Digital Marketplace buyer account holders page
 
--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -6,7 +6,7 @@ import json
 
 
 def random_string():
-    return ''.join(random.choice(string.lowercase) for x in range(10))
+    return ''.join(random.choice(string.lowercase) for x in range(20))
 
 
 def logout(browser):
@@ -16,13 +16,11 @@ def logout(browser):
 def login(browser, email, password):
     logout(browser)
     browser.visit('{0}{1}'.format(config['DM_FRONTEND_URL'], '/login'))
+    browser.is_element_present_by_name('email_address', config['DM_SELENIUM_WAIT_TIME'])
     browser.fill('email_address', email)
     browser.fill('password', password)
     click_button('Log in', browser)
-    if browser.title.startswith('Terms of Use Updated'):
-        accept_checkbox = browser.find_by_xpath("//label[text()='I accept the updated Terms of Use']").first
-        accept_checkbox.click()
-        click_button('Accept', browser)
+    wait_for_page('Dashboard', browser)
 
 
 def click_button(text, browser):
@@ -54,9 +52,12 @@ def enter_title(title, browser):
 def create_brief(title, browser):
     visit_page('/', browser)
     click_link('Find an individual specialist', browser)
+    wait_for_page('Research, write and publish your brief', browser)
     click_button('Create brief', browser)
+    wait_for_page('Create a title for your brief', browser)
     enter_title(title, browser)
     click_button('Save and continue', browser)
+    wait_for_page('quick and easy to publish a brief', browser)
 
 
 def delete_brief(title):
@@ -68,3 +69,15 @@ def delete_brief(title):
     if briefs:
         requests.delete('{}/briefs/{}'.format(config['DM_DATA_API_URL'], briefs[0]['id']), headers=headers,
                         data=json.dumps({'updated_by': ''}))
+
+
+def wait_for_page(page_text, browser):
+    for i in range(0, 3):
+        try:
+            result = browser.is_text_present(page_text, config['DM_SELENIUM_WAIT_TIME'])
+        except Exception as exception:
+            if type(exception).__name__ == 'StaleElementReferenceException':
+                continue
+            raise
+        assert result
+        break
