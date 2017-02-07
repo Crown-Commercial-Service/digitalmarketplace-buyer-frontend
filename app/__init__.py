9f92afa9c804db275bbe3ef489d6aaf648f41f2a
David McDonald <idavidmcdonald@me.com>
2017-02-07 17:15:21 +0000

    Load DOS2 display brief manifest

--- app/__init__.py
+++ app/__init__.py
@@ -19,6 +19,7 @@ content_loader = ContentLoader('app/content')
 content_loader.load_manifest('g-cloud-6', 'services', 'search_filters')
 content_loader.load_manifest('g-cloud-6', 'services', 'display_service')
 content_loader.load_manifest('digital-outcomes-and-specialists', 'briefs', 'display_brief')
+content_loader.load_manifest('digital-outcomes-and-specialists-2', 'briefs', 'display_brief')
 
 
 def create_app(config_name):
