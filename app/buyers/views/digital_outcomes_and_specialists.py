1dc6c6ffcaf46d75d46edaefd590e1ea94873648
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2017-02-15 15:54:51 +0000

    Multiple statues when getting framework and lot

--- app/buyers/views/digital_outcomes_and_specialists.py
+++ app/buyers/views/digital_outcomes_and_specialists.py
@@ -11,7 +11,7 @@ from ..helpers.buyers_helpers import get_framework_and_lot
 @dos.route('/frameworks/<framework_slug>/requirements/user-research-studios', methods=['GET'])
 def studios_start_page(framework_slug):
     # Check framework is live and has the user-research-studios lot
-    get_framework_and_lot(framework_slug, 'user-research-studios', data_api_client, status='live')
+    get_framework_and_lot(framework_slug, 'user-research-studios', data_api_client, allowed_statuses='live')
 
     return render_template(
         "buyers/studios_start_page.html"
@@ -21,7 +21,7 @@ def studios_start_page(framework_slug):
 @dos.route('/frameworks/<framework_slug>/requirements/<lot_slug>', methods=['GET'])
 def info_page_for_starting_a_brief(framework_slug, lot_slug):
     framework, lot = get_framework_and_lot(framework_slug, lot_slug, data_api_client,
-                                           status='live', must_allow_brief=True)
+                                           allowed_statuses='live', must_allow_brief=True)
     return render_template(
         "buyers/start_brief_info.html",
         framework=framework,
