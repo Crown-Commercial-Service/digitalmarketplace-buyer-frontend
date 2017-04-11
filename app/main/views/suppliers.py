c5340529111a11074afa7ee3e0e6b7e6b04daf6a
Alex Sadleir <alex.sadleir@digital.gov.au>
2017-04-11 12:25:57 +1000

    Add details about panels/SONs to profile

--- app/main/views/suppliers.py
+++ app/main/views/suppliers.py
@@ -61,6 +61,12 @@ def get_supplier(code):
         props = {"application": {key: supplier[key] for key in supplier if key not in ['disclosures']}}
         props['application']['case_study_url'] = '/case-study/'
         props['application']['public_profile'] = not owns_profile
+        props['application']['digital_marketplace_panel'] = False
+        digital_marketplace_framework = data_api_client.req.frameworks('digital-marketplace').get()
+        for framework in supplier.get('frameworks', []):
+            if framework['framework_id'] == digital_marketplace_framework['frameworks']['id']:
+                props['application']['digital_marketplace_panel'] = True
+        props['application']['dsp_panel'] = len(supplier.get('domains', {'legacy': []})['legacy']) > 0
         props['basename'] = url_for('.get_supplier', code=code)
         props['form_options'] = {
             'action': "/sellers/edit",
