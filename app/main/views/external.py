081358d0978b42f6d30a48b0d5bfff508339bfeb
Alexey Bezhan <al@fueledbylemons.com>
2017-06-19 14:18:46 +0100

    Add external views for brief-frontend endpoints
    Adds buyer dashboard and DOS lot start pages to the external views.
    
    The URLs need an explicit `/buyers/` prefix since the blueprint doesn't
    have a common `url_prefix` set.

--- app/main/views/external.py
+++ app/main/views/external.py
@@ -1,6 +1,11 @@
 from .. import external
 
 
+@external.route('/buyers')
+def buyer_dashboard():
+    raise NotImplementedError()
+
+
 @external.route('/buyers/create')
 def create_buyer_account():
     raise NotImplementedError()
@@ -8,4 +13,14 @@ def create_buyer_account():
 
 @external.route('/buyers/create')
 def submit_create_buyer_account():
+    raise NotImplementedError()
+
+
+@external.route('/buyers/frameworks/<framework_slug>/requirements/<lot_slug>')
+def info_page_for_starting_a_brief(framework_slug, lot_slug):
+    raise NotImplementedError()
+
+
+@external.route('/buyers/frameworks/<framework_slug>/requirements/user-research-studios')
+def studios_start_page(framework_slug):
     raise NotImplementedError()
