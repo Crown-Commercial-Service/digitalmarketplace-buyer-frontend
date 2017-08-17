8bb9b35b6ab238c5a162d6fabe23f5d1da30acc9
Alexander Sadleir <maxious@users.noreply.github.com>
2017-08-17 15:15:07 +1000

    Back to top links (#623)

--- app/assets/javascripts/_main.js
+++ app/assets/javascripts/_main.js
@@ -1,16 +1,4 @@
 $(document).ready(function() {
-  var offset = 250;
-
-  var duration = 300;
-
-  jQuery(window).scroll(function() {
-    if (jQuery(this).scrollTop() > offset) {
-      jQuery('.scroll-to-top').fadeIn(duration);
-    } else {
-      jQuery('.scroll-to-top').fadeOut(duration);
-    }
-  });
-
   jQuery('.scroll-to-top').click(function(e){
     e.preventDefault();
     jQuery('html, body').animate({scrollTop : 0},800);
