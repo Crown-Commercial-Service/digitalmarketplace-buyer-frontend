3513b908e38d4618bbf49aa90d1211d893031046
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-09-22 15:36:42 +0100

    Data tracking for new flow
    As a member of the Digital Marketplace team
    I need to measure how people use the save search feature
    so that I can make decisions to iterate and improve it
    
    Ticket: https://trello.com/c/eo8gJE4C/713-data-tracking-for-new-flow

--- app/assets/javascripts/analytics/_events.js
+++ app/assets/javascripts/analytics/_events.js
@@ -97,8 +97,26 @@
         });
       }
     },
+    trackEvent: function (e) {
+      var $target = $(e.target);
+      var category = $target.attr('data-analytics-category');
+      var action = $target.attr('data-analytics-action');
+      var label = $target.attr('data-analytics-label');
+      
+      GOVUK.GDM.analytics.events.sendEvent(category, action, label);
+      
+    },
+    sendEvent: function (category, action, label) {
+      GOVUK.analytics.trackEvent(category, action, {
+        'label': label,
+        'transport': 'beacon'
+      });
+    },
     'init': function () {
-      $('body').on('click', 'a', this.supplierListDownload)
+      // TODO: this should be within a specific class and only initialised on the supplier list download page
+      // Currently this is running on click of any <a> link!
+      $('body').on('click', 'a', this.supplierListDownload);
+      $('body').on('click', '[data-analytics=trackEvent]', this.trackEvent);
     }
   };
 })(window, window.GOVUK);
