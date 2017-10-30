2f658226947831eff69b25d39c537429200ac768
Alex Sadleir <alex.sadleir@digital.gov.au>
2017-10-31 08:31:33 +1100

    fix test

--- tests/app/views/test_login.py
+++ tests/app/views/test_login.py
@@ -82,7 +82,8 @@ class TestLogin(BaseApplicationTest):
                 'csrf_token': FakeCsrf.valid_token,
             })
             assert res.status_code == 302
-            assert res.location == 'http://localhost' + self.expand_path('/digital-marketplace/opportunities?status=live')
+            assert res.location == 'http://localhost' + \
+                                   self.expand_path('/digital-marketplace/opportunities?status=live')
             assert 'Secure;' in res.headers['Set-Cookie']
 
     def test_should_redirect_logged_in_buyer(self):
