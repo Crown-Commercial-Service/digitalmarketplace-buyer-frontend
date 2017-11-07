b501f9b1639bed576bd4449d1a8163ddf1cbcb96
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2017-11-07 16:56:39 +0000

    Track >100 services error on end your search
    We want to answer these questions:
    
    - how many users see this error?
    
    - what do users do after they see the error?
    
    - how many services are in the list when users try to end their search?
    
    Ticket: https://trello.com/c/s2Xp5iE3/168-track-100-services-error-on-end-your-search

--- app/assets/javascripts/analytics/_virtualPageViews.js
+++ app/assets/javascripts/analytics/_virtualPageViews.js
@@ -16,14 +16,23 @@
 
   };
 
-  VirtualPageViews.prototype.sendVirtualPageView = function( url, name ) {
+  VirtualPageViews.prototype.sendVirtualPageView = function( url, name, dimensions ) {
 
-    if ( GOVUK.analytics  && url ) {
-      var urlList = url.split("?");
-      urlList[0]  = urlList[0] + "/vpv";
-      url         = urlList.join("?");
-      GOVUK.analytics.trackPageview(url, name);
+    if ( !GOVUK.analytics || !url ) return false;
+
+    if ( dimensions )
+    {
+      for ( var i = 0; i < dimensions.length; i++ )
+      {
+        var dimension = dimensions[i];
+        GOVUK.analytics.setDimension(dimension.id, dimension.label);
+      }
     }
+
+    var urlList = url.split("?");
+    urlList[0]  = urlList[0] + "/vpv";
+    url         = urlList.join("?");
+    GOVUK.analytics.trackPageview(url, name);
 
   };
 
