73c7699f12b3a4bf780380878652e50c005e8668
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-02-26 16:53:49 +0000

    Remove duplicated analytics JS that's now supported by FE toolkit

--- spec/javascripts/manifest.js
+++ spec/javascripts/manifest.js
@@ -5,7 +5,7 @@ var manifest = {
     '../../../node_modules/scrolldepth/jquery.scrolldepth.js',
     '../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/google-analytics-universal-tracker.js',
     '../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/analytics/analytics.js',
-    '../../../app/assets/javascripts/analytics/_register.js',
+    '../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_register.js',
     '../../../app/assets/javascripts/analytics/_pageViews.js',
     '../../../app/assets/javascripts/analytics/_events.js',
     '../../../app/assets/javascripts/analytics/_virtualPageViews.js',
