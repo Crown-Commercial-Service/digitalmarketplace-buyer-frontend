ff3ff4a3d3b7897c60aed67c93c244f732ada376
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2018-02-27 16:38:31 +0000

    Add banner to account page
    Add the user research banner to the account page using the existing GOV.uk pattern. Allow for links to 'sign up to mailing list' page
    
    As a  user I should be presented with thte banner to sign up to the user research mailing list when I log in if: I have not already closed it and I have not already signed up for this.
    This is the first point of contact for getting users to sign up to the mailing list.
    
    The close link should set a cookie value of 'seen_this_banner_thing' to true so we don't show it again
    The sign up link should link to the new sign up page.
    
    https://trello.com/c/OJ9lFk1M/129-add-banner-to-account-page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,7 @@
 //= require ../../../node_modules/govuk_frontend_toolkit/javascripts/govuk/shim-links-with-button-role.js
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/shim-links-with-button-role.js
 //= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/show-hide-content.js
+//= require ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/user-research-consent-banner.js
 //= require _analytics.js'
 //= require _onready.js'
 //= require _selection-buttons.js
