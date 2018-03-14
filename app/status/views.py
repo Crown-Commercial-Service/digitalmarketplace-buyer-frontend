09d84de9ccf4afde103a3b86259e1429bbaf891b
Samuel Williams <Samuel.Williams@digital.cabinet-office.gov.uk>
2018-03-14 14:45:59 +0000

    Use centralised status method
     ## Summary
    The status endpoint for all of the apps is highly redundant across
    repositories so I've moved it to dm-utils. We now only need to call this
    method from the view stub in each app and pass in a couple of resources
    (mainly the current_app and any api clients) to get a consistent status
    JSON blob back.
    
     ## Ticket
    https://trello.com/c/BBvu6eUk/383-ensure-healthcheck-endpoint-fails-if-low-disk-space

--- app/status/views.py
+++ app/status/views.py
@@ -1,54 +1,12 @@
-from flask import jsonify, current_app, request
+from flask import request
 
 from . import status
 from .. import data_api_client, search_api_client
-from dmutils.status import get_flags
+from dmutils.status import get_app_status
 
 
 @status.route('/_status')
 def status():
-
-    if 'ignore-dependencies' in request.args:
-        return jsonify(
-            status="ok",
-        ), 200
-
-    version = current_app.config['VERSION']
-    apis = [
-        {
-            'name': '(Data) API',
-            'key': 'api_status',
-            'status': data_api_client.get_status()
-        }, {
-            'name': 'Search API',
-            'key': 'search_api_status',
-            'status': search_api_client.get_status()
-        }
-    ]
-
-    apis_with_errors = []
-
-    for api in apis:
-        if api['status'] is None or api['status']['status'] != "ok":
-            apis_with_errors.append(api['name'])
-
-    # if no errors found, return as is.  Else, return an error and a message
-    if not apis_with_errors:
-        return jsonify(
-            {api['key']: api['status'] for api in apis},
-            status="ok",
-            version=version,
-            flags=get_flags(current_app)
-        )
-
-    message = "Error connecting to the {}.".format(
-        " and the ".join(apis_with_errors)
-    )
-
-    return jsonify(
-        {api['key']: api['status'] for api in apis},
-        status="error",
-        version=version,
-        message=message,
-        flags=get_flags(current_app)
-    ), 500
+    return get_app_status(data_api_client=data_api_client,
+                          search_api_client=search_api_client,
+                          ignore_dependencies='ignore-dependencies' in request.args)
