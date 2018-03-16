796a552beb772770a6be458d2268d39abf6689b2
Nathan Davison <nathan.davison@digital.gov.au>
2018-03-16 12:02:04 +1100

    Remove the -U from pip install for the CI tests/builds.

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -3,6 +3,6 @@ set -xe
 npm install
 ./node_modules/.bin/pancake
 pip install --upgrade pip setuptools
-pip install -U -r requirements_for_test.txt
+pip install -r requirements_for_test.txt
 npm run frontend-build:production
 git log --pretty=format:'%h' -n 1 > version_label
