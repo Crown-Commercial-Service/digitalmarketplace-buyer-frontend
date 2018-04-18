12581c51331f0613fe12e76730990b89cb49a35b
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-04-18 16:47:25 +0100

    Refactor test_errors (API client patching part 6)

--- tests/main/views/test_errors.py
+++ tests/main/views/test_errors.py
@@ -6,16 +6,18 @@ from dmapiclient import HTTPError
 from ...helpers import BaseApplicationTest
 
 
-@mock.patch('app.main.views.g_cloud.search_api_client', autospec=True)
 class TestErrors(BaseApplicationTest):
 
     def setup_method(self, method):
         super().setup_method(method)
+        self.search_api_client_patch = mock.patch('app.main.views.g_cloud.search_api_client', autospec=True)
+        self.search_api_client = self.search_api_client_patch.start()
 
     def teardown_method(self, method):
+        self.search_api_client_patch.stop()
         super().teardown_method(method)
 
-    def test_404(self, search_api_mock):
+    def test_404(self):
         res = self.client.get('/g-cloud/service/1234')
         assert res.status_code == 404
         assert "Check you've entered the correct web " \
@@ -28,7 +30,7 @@ class TestErrors(BaseApplicationTest):
             "enquiries@digitalmarketplace.service.gov.uk</a>" \
             in res.get_data(as_text=True)
 
-    def test_410(self, search_api_mock):
+    def test_410(self):
         res = self.client.get('/digital-services/framework')
         assert res.status_code == 410
         assert "Check you've entered the correct web " \
@@ -41,12 +43,12 @@ class TestErrors(BaseApplicationTest):
             "enquiries@digitalmarketplace.service.gov.uk</a>" \
             in res.get_data(as_text=True)
 
-    def test_500(self, search_api_mock):
+    def test_500(self):
         self.app.config['DEBUG'] = False
 
         api_response = mock.Mock()
         api_response.status_code = 503
-        search_api_mock.search.side_effect = HTTPError(api_response)
+        self.search_api_client.search.side_effect = HTTPError(api_response)
 
         res = self.client.get('/g-cloud/search?q=email')
         assert res.status_code == 503
