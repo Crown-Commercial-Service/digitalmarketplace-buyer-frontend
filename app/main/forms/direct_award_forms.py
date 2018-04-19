3f56914ed8bffff48915364afebcf7db28f03f99
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2018-04-19 13:40:04 +0100

    Rename Form to FlaskForm

--- app/main/forms/direct_award_forms.py
+++ app/main/forms/direct_award_forms.py
@@ -1,10 +1,10 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms.validators import Length, Optional
 
 from dmutils.forms import StripWhitespaceStringField
 
 
-class CreateProjectForm(Form):
+class CreateProjectForm(FlaskForm):
     name = StripWhitespaceStringField(
         'Name your search', id="project_name",
         validators=[
