a9d91584ce3995006d846e0dc03a3928ff433b5a
malmas18 <29763502+malmas18@users.noreply.github.com>
2018-07-24 13:22:51 +1000

    Try tro fix the build (#757)
    update ci_build.sh
    use older version weasy print

--- scripts/ci_build.sh
+++ scripts/ci_build.sh
@@ -3,6 +3,7 @@ set -xe
 npm install
 ./node_modules/.bin/pancake
 pip install --upgrade pip setuptools
+pip install -r requirements.txt
 pip install -r requirements_for_test.txt
 npm run frontend-build:production
 git log --pretty=format:'%h' -n 1 > version_label
