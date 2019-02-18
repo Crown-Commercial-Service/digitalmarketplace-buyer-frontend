6fec2b8f5428e45f90bec293037b977f4254510b
Nathan <nathan.davison@digital.gov.au>
2019-02-18 16:07:04 +1100

    Ask the Market (#809)
    * [Overview] Changed labels and links when work order exists
    
    * Add ATM to redirects.
    
    * Add ATM draft template file.
    
    * Update frameworks commit.
    
    * Update to latest frameworks master commit.

--- app/main/views/marketplace.py
+++ app/main/views/marketplace.py
@@ -106,7 +106,7 @@ def _is_supplier_selected_for_brief(brief):
 def get_brief_by_id(framework_slug, brief_id):
     briefs = data_api_client.get_brief(brief_id)
     brief = briefs.get('briefs')
-    if brief['lotSlug'] == 'rfx':
+    if brief['lotSlug'] in ['rfx', 'atm']:
         return redirect('/2/%s/opportunities/%s' % (framework_slug, brief_id), 301)
     if brief['status'] not in ['live', 'closed']:
         if not current_user.is_authenticated or brief['users'][0]['id'] != current_user.id:
