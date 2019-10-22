363042c0bfee69bbfb9844545cec6cf6dd53da70
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2019-10-22 14:49:25 +0100

    Update Gulp from 3.8.11 to 4.0.2
    Main differences were that `gulp.start` is depricated in v4 and v4 introduces
    gulp.series/gulp.parallel to declare a list of tasks and whether they can
    run in series or parallel.
    
    I also had to update gulp-sourcemaps from 1.5.2 to 2.6.5 to fix issues
    with vinyl-fs when running the js gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -262,19 +262,6 @@ gulp.task('test', function () {
     }))
 })
 
-gulp.task('watch', ['build:development'], function () {
-  const jsWatcher = gulp.watch([path.join(assetsFolder, '**', '*.js')], ['js'])
-  const cssWatcher = gulp.watch([path.join(assetsFolder, '**', '*.scss')], ['sass'])
-  const dmWatcher = gulp.watch([path.join(npmRoot, 'digitalmarketplace-frameworks', '**')], ['copy:frameworks'])
-  const notice = function (event) {
-    console.log('File ' + event.path + ' was ' + event.type + ' running tasks...')
-  }
-
-  cssWatcher.on('change', notice)
-  jsWatcher.on('change', notice)
-  dmWatcher.on('change', notice)
-})
-
 gulp.task('set_environment_to_development', function (cb) {
   environment = 'development'
   cb()
@@ -285,38 +272,35 @@ gulp.task('set_environment_to_production', function (cb) {
   cb()
 })
 
-gulp.task(
-  'copy',
-  [
-    'copy:frameworks',
-    'copy:template_assets:images',
-    'copy:template_assets:stylesheets',
-    'copy:template_assets:javascripts',
-    'copy:govuk_toolkit_assets:images',
-    'copy:dm_toolkit_assets:stylesheets',
-    'copy:dm_toolkit_assets:images',
-    'copy:dm_toolkit_assets:templates',
-    'copy:images',
-    'copy:svg',
-    'copy:govuk_template'
-  ]
-)
+gulp.task('copy', gulp.parallel(
+  'copy:frameworks',
+  'copy:template_assets:images',
+  'copy:template_assets:stylesheets',
+  'copy:template_assets:javascripts',
+  'copy:govuk_toolkit_assets:images',
+  'copy:dm_toolkit_assets:stylesheets',
+  'copy:dm_toolkit_assets:images',
+  'copy:dm_toolkit_assets:templates',
+  'copy:images',
+  'copy:svg',
+  'copy:govuk_template'
+))
 
-gulp.task(
-  'compile',
-  [
-    'copy'
-  ],
-  function () {
-    gulp.start('sass')
-    gulp.start('js')
+gulp.task('compile', gulp.series('copy', gulp.parallel('sass', 'js')))
+
+gulp.task('build:development', gulp.series(gulp.parallel('set_environment_to_development', 'clean'), 'compile'))
+
+gulp.task('build:production', gulp.series(gulp.parallel('set_environment_to_production', 'clean'), 'compile'))
+
+gulp.task('watch', gulp.series('build:development', function () {
+  const jsWatcher = gulp.watch([path.join(assetsFolder, '**', '*.js')], gulp.series('js'))
+  const cssWatcher = gulp.watch([path.join(assetsFolder, '**', '*.scss')], gulp.series('sass'))
+  const dmWatcher = gulp.watch([path.join(npmRoot, 'digitalmarketplace-frameworks', '**')], gulp.series('copy:frameworks'))
+  const notice = function (event) {
+    console.log('File ' + event.path + ' was ' + event.type + ' running tasks...')
   }
-)
 
-gulp.task('build:development', ['set_environment_to_development', 'clean'], function () {
-  gulp.start('compile')
-})
-
-gulp.task('build:production', ['set_environment_to_production', 'clean'], function () {
-  gulp.start('compile')
-})
+  cssWatcher.on('change', notice)
+  jsWatcher.on('change', notice)
+  dmWatcher.on('change', notice)
+}))
