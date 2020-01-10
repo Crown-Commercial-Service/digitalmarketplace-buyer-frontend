f8d213628ced7383ba86cdd9d8a29bd4670b0dd9
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2020-01-10 14:34:15 +0000

    Remove unused JavaScript and Sass
    I removed report-a-problem.js because we are not actually using that
    component any more but still importing it.
    
    I remove user-research-consent-banner.js because we will not longer be
    using the user research banner and it also requires GOVUK.Cookie js
    helpers which come from GOVUK Template. We will be implementing
    our own cookie methods but for the time being we have disabled all
    cookie js.
    
    Removed analytics JavaScript as it will be replaced soon and is
    currently disabled.
    
    Remove govuk-elements imports and styles for older versions of Internet
    Explorer from the stylesheet.

--- app/assets/javascripts/_analytics.js
+++
@@ -1,8 +0,0 @@
-//= include ../../../node_modules/digitalmarketplace-frontend-toolkit/toolkit/javascripts/analytics/_init.js
-
-// TODO: move remaining shared analytics code into FE toolkit
-//= include analytics/_pageViews.js
-//= include analytics/_buyerSpecificEvents.js
-//= include analytics/_virtualPageViews.js
-//= include analytics/_scrollTracking.js
-//= include analytics/_init.js
