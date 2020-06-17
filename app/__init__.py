676bce2309b84586fd84c145e7f0697e1c8184e2
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-06-17 10:58:45 +0100

    Disable FlaskLogin flash messages
    Previously we've been ignoring flash messages from flask-login in our
    flash messages template (see [`toolkit/flash_messages.html#L6`][1]).
    
    We should instead be stopping flask-login from sending these flash
    messages in the first place; setting `LoginManager.login_message` to
    `None` should do this (see [`LoginManager.unauthorized()`][2]).
    
    The ability to suppress flash messages in this way was introduced in
    flask-login version 0.1.1. See pull request maxcountryman/flask-login#1.
    
    [1]: https://github.com/alphagov/digitalmarketplace-frontend-toolkit/blob/master/toolkit/templates/flash_messages.html#L6
    [2]: https://github.com/maxcountryman/flask-login/blob/0.4.1/flask_login/login_manager.py#L124-L177

--- app/__init__.py
+++ app/__init__.py
@@ -107,7 +107,7 @@ def create_app(config_name):
     application.register_blueprint(external_blueprint)
 
     login_manager.login_view = '/user/login'
-    login_manager.login_message_category = "must_login"
+    login_manager.login_message = None  # don't flash message to user
     gds_metrics.init_app(application)
     csrf.init_app(application)
 
